4ef2835f40286c154dcfa32773bb46ba62985184
Oty_v <oty998@gmail.com>
2021-04-01 10:07:07 +0300

    use apiResource

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,4 @@ use App\Http\Controllers\PostController;
 |
 */
 
-Route::prefix('posts')->group(function () {
-    Route::get('/', [PostController::class, 'index']);
-    Route::get('/{post}', [PostController::class, 'show']);
-    Route::post('/', [PostController::class, 'store']);
-    Route::put('/{post}', [PostController::class, 'update']);
-    Route::delete('/{post}', [PostController::class, 'destroy']);
-});
+Route::apiResource('posts', PostController::class);
