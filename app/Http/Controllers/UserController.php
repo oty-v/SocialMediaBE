b4d8c9c50ea083539af52fa61dd3fdf3f29444c5
Oty_v <oty998@gmail.com>
2021-04-02 11:50:25 +0300

    login of users and issuance of tokens using sanctum

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,4 +25,19 @@ class UserController extends Controller
         return response(['user' => $user, 'token' => $token], 201);
     }
 
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+
+        if (!auth()->attempt($fields)) {
+            return response(['message' => 'This User does not exist, check your details'], 400);
+        }
+
+        $token = auth()->user()->createToken('authToken')->plainTextToken;
+
+        return response(['user' => auth()->user(), 'access_token' => $token], 201);
+    }
 }
