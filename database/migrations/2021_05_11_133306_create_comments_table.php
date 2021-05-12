3b59902d3791a09038022b4b1a8e4719d6f37f42
Oty_v <oty998@gmail.com>
2021-05-12 17:33:26 +0300

    add onDelete cascade

--- database/migrations/2021_05_11_133306_create_comments_table.php
+++ database/migrations/2021_05_11_133306_create_comments_table.php
@@ -16,7 +16,7 @@ class CreateCommentsTable extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained();
-            $table->foreignId('post_id')->constrained();
+            $table->foreignId('post_id')->constrained()->onDelete('cascade');
             $table->text('body');
             $table->timestamps();
         });
