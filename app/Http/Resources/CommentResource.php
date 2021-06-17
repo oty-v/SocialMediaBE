70271be6af767b5e8af329ab69daac29dccda33d
Oty_v <oty998@gmail.com>
2021-06-17 15:50:09 +0300

    edit resource

--- app/Http/Resources/CommentResource.php
+++ app/Http/Resources/CommentResource.php
@@ -18,7 +18,7 @@ class CommentResource extends JsonResource
             'id' => $this->id,
             'author' => new UserResource($this->user),
             'tags' => TagResource::collection($this->tags),
-            'mentions' => UserResource::collection($this->mentionedUsers),
+            'mentionedUsers' => UserResource::collection($this->mentionedUsers),
             'content' => $this->content,
             'created_at' => $this->created_at,
         ];
